e88bb7672199546e398c0e865a25e5e59095be60
Carlos <carloshlzansavio@gmail.com>
2022-08-05 12:15:33 +0100

    feat: add service tests

--- src/uploader/uploader.service.spec.ts
+++ src/uploader/uploader.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BlobService } from '@czarpoliedros/blob';
 import { UploaderService } from './uploader.service';
+import { PassThrough } from 'stream';
+import { getModelToken } from '@nestjs/mongoose';
+import { Image } from './entities/image.schema';
 
 describe('UploaderService', () => {
   let service: UploaderService;
+  const mockImageModel = jest.fn();
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [UploaderService],
-  //   }).compile();
+  beforeEach(async () => {
+    jest.resetAllMocks();
 
-  //   service = module.get<UploaderService>(UploaderService);
-  // });
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UploaderService,
+        {
+          provide: BlobService,
+          useValue: {
+            uploadBufferFile: jest.fn(),
+          },
+        },
+        {
+          provide: getModelToken(Image.name),
+          useValue: mockImageModel,
+        },
+      ],
+    }).compile();
 
-  it('should be defined', () => {
-    expect(true).toBeTruthy();
+    service = module.get<UploaderService>(UploaderService);
+  });
+
+  it('should save image', async () => {
+    mockImageModel.mockReturnValue({ save: jest.fn(), id: '123' });
+
+    const file: Express.Multer.File = {
+      fieldname: 'image.jpg',
+      filename: 'image.jpg',
+      size: 12,
+      originalname: 'image.jpg',
+      encoding: '',
+      mimetype: '',
+      buffer: new Buffer('23, 32'),
+      destination: '',
+      path: '',
+      stream: new PassThrough(),
+    };
+
+    const response = await service.saveImage(
+      'image',
+      'a good description',
+      file,
+    );
+
+    expect(response).toEqual('123');
   });
 });
